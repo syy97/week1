9063f443a3f746202e89ce26e924b55ddcb63e5a
shiyaoyu <shiyaoyu>
2019-02-18 09:56:27 +0800

    ss

--- gulpfile.js
+++ gulpfile.js
@@ -7,50 +7,50 @@ var webserver = require("gulp-webserver");
 
 //开启服务
 gulp.task("webserver", function() {
-    return gulp.src("./src")
-        .pipe(webserver() {
+    return gulp.src("./")
+        .pipe(webserver({
             open: true,
             port: 8080,
             livereload: true
-        })
+        }))
 })
 
 //scss
 gulp.task("sass", function() {
     return gulp.src("./src/css/*.scss")
         .pipe(sass())
-        .pipe(gulp.dest("./dist"))
+        .pipe(gulp.dest("./src/css"))
 })
 
 //合并压缩
 gulp.task("sass", function() {
-    return gulp.src("./src")
+    return gulp.src("./src/css/*.scss")
         .pipe(concat("main.css"))
         .pipe(cssmin())
-        .pipe(gulp.dest("./dist"))
+        .pipe(gulp.dest("./dist/css"))
 })
 
 //js
 gulp.task("js", function() {
     return gulp.src("./src/js/*.js")
-        .pipe(concat("./dulid.js"))
+        .pipe(concat("dulid.js"))
         .pipe(jsmin())
-        .pipe(gulp.dest("./dist"))
+        .pipe(gulp.dest("./dist/js"))
 })
 
 //监听watch
 gulp.task("watch", function() {
-    return gulp.watch("./src/*.css")
+    return gulp.watch("./src/css/*.css", gulp.series("sass", "css"));
 
 })
 
 //创建default任务
-gulp.task("default", function() {
-    return gulp.series("sass", "css", "webserver", "watch")
-})
+gulp.task("default",
+    gulp.series("sass", "css", "webserver", "watch")
+)
 
 //创建build任务
-gulp.task("build", function() {
-    return gulp.parallel("js", "css")
-        .pipe(gulp.dest("./dist"))
-})
+gulp.task("build",
+    gulp.parallel("js", "css")
+    .pipe(gulp.dest("./dist"))
+)
